539d3de1dd946771d2e79d9aa0ac605b446dfddc
spf123000 <spf123000177010@aliyun.com>
2020-11-20 17:26:29 +0800

    实现删除和编辑

--- purchase/commodity/urls.py
+++ purchase/commodity/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from commodity import views
 
 urlpatterns = [
-    path('', views.commodity),
+    path('', views.index),
+    path('show/', views.commodity_show),
+    path('delete/', views.commodity_delete),
+    path('modify/', views.commodity_modify),
 ]
--- purchase/commodity/views.py
+++ purchase/commodity/views.py
@@ -1,20 +1,59 @@
 from django.shortcuts import render
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import Http404
 from . import models
 import json
 
 # Create your views here.
 
-def commodity(request):
-    if request.is_ajax() and request.method == "POST":
-        pass
+def index(request):
+    
+    return render(request, 'commodity.html')
 
-
-
-
+def commodity_show(request):
+    total = models.commodity.objects.count()
     items = models.commodity.objects.values()
-    return render(request, 'commodity.html', {'items': json.dumps(list(items))})
+    rows = []
+    for item in items:
+        rows.append({
+            'commodity_serial' : item['commodity_serial'],
+            'commodity_id' : item['commodity_id'],
+            'commodity_link' : item['commodity_link'],
+            'commodity_name' : item['commodity_name'],
+            'commodity_num' : item['commodity_num'],
+            'commodity_price' : item['commodity_price'],
+            'commodity_total' : item['commodity_total'],
+        })
+    data = {'total': total, 'rows': rows}
+    # return render(request, 'commodity.html', data)
+    return HttpResponse(json.dumps(data), content_type='application/json')
 
-def commodity_save(request):
-    if request.is_ajax():
-        commodity_id = request.GET.getlist
+
+def commodity_delete(request):
+    if request.is_ajax():        
+        commodity_serial = request.GET.getlist('id[]')
+        for i in range(len(commodity_serial)):
+            models.commodity.objects.filter(commodity_serial=commodity_serial[i]).delete()
+        data = {'ret': True}
+        return HttpResponse(json.dumps(data), content_type='application/json')
+    else:
+        raise Http404("Not Found")
+
+def commodity_modify(request):
+    if request.is_ajax():        
+        commodity_serial = request.GET.get('id')
+        print(commodity_serial)
+        commodity_num = request.GET.get('num')
+        commodity_price = request.GET.get('price')
+        print(commodity_price)
+        item = models.commodity.objects.get(commodity_serial=commodity_serial)
+        item.commodity_num = commodity_num
+        item.commodity_price = commodity_price
+        item.save()
+        data = {
+            'ret': True,            
+        }
+ 
+        return HttpResponse(json.dumps(data), content_type='application/json')
+    else:
+        return Http404("Not Found")
